2a1e14fb53dcc52057c9b57ad29f182557e3ff53
ORI-Muchim <whals467746@gmail.com>
2023-06-27 02:42:14 +0900

    Edit inference.py

--- inference.py
+++ inference.py
@@ -1,21 +1,31 @@
 # -*- coding: utf-8 -*-
 import sys
-import os
+import subprocess
+import vits.utils
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python inference.py {model_name} {model_step}")
+    if len(sys.argv) != 2:
+        print("Usage: python main.py {model_name}")
         sys.exit(1)
 
+    hps = vits.utils.get_hparams()
+
     model_name = sys.argv[1]
-    model_step = sys.argv[2]
+    model_step = vits.utils.load_checkpoint(vits.utils.latest_checkpoint_path(hps.model_dir, "G_*.pth"))
 
     command = f"python ./vits/inferencems.py {model_name} {model_step}"
 
-    return_code = os.system(command)
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    
+    for line in iter(process.stdout.readline, b''):
+        print(line.decode('utf-8'), end='')
+        
+    process.stdout.close()
+    process.wait()
 
-    if return_code != 0:
+    if process.returncode != 0:
         print("Error occurred")
 
 if __name__ == "__main__":
     main()
+
