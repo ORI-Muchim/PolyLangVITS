e49d93fcc9f88ccfe5d60d9f3f45d7200b8ffb3d
ORI-Muchim <whals467746@gmail.com>
2023-06-28 01:54:43 +0900

    Edit main.py

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ os.chdir('./datasets')
 subprocess.run(["python", "integral.py", sys.argv[1], sys.argv[2], sys.argv[3]])
 
 os.chdir('../')
-subprocess.run(["python", "get_pretrained_model.py", sys.argv[2]])
+subprocess.run(["python", "get_pretrained_model.py", sys.argv[1], sys.argv[2]])
 
 config_path = f"../datasets/{sys.argv[2]}.json"
 model_name = sys.argv[2]
 
-os.chdir('./datasets')
-
-os.chdir('../vits')
+os.chdir('./vits')
 command = ["python", "train_ms.py", "-c", config_path, "-m", model_name]
 
 subprocess.run(command)
