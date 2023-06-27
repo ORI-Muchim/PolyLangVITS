18b815efb0d0bcf4c4ded4708977e2ff349ba2d4
ORI-Muchim <whals467746@gmail.com>
2023-06-28 01:48:49 +0900

    Edit main.py

--- main_low.py
+++ main_low.py
@@ -6,7 +6,7 @@ os.chdir('./datasets')
 subprocess.run(["python", "integral_low.py", sys.argv[1], sys.argv[2], sys.argv[3]])
 
 os.chdir('../')
-subprocess.run(["python", "get_pretrained_model.py", sys.argv[2]])
+subprocess.run(["python", "get_pretrained_model.py", sys.argv[1], sys.argv[2]])
 
 config_path = f"../datasets/{sys.argv[2]}.json"
 model_name = sys.argv[2]
