e283f411f23bb9ab3ce4b8b644cf989b92a98f25
ORI-Muchim <whals467746@gmail.com>
2023-06-28 00:48:23 +0900

    Add get_pretrained_model.py

---
+++ get_pretrained_model.py
@@ -0,0 +1,23 @@
+import requests
+import sys
+import os
+
+
+url1 = 'https://github.com/ORI-Muchim/PolyLangVITS/releases/download/v1.0/D_0.pth'
+url2 = 'https://github.com/ORI-Muchim/PolyLangVITS/releases/download/v1.0/G_0.pth'
+response1 = requests.get(url1, allow_redirects=True)
+response2 = requests.get(url2, allow_redirects=True)
+
+directory = f'./models/{sys.argv[2]}'
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+discriminator_model = os.path.join(directory, 'D_0.pth')
+generator_model = os.path.join(directory, 'G_0.pth')
+
+with open(discriminator_model, 'wb') as file:
+    file.write(response1.content)
+
+with open(generator_model, 'wb') as file:
+    file.write(response2.content)
--- main_low.py
+++ main_low.py
@@ -5,8 +5,13 @@ import sys
 os.chdir('./datasets')
 subprocess.run(["python", "integral_low.py", sys.argv[1], sys.argv[2], sys.argv[3]])
 
+os.chdir('../')
+command = ["python", "get_pretrained_model.py", sys.argv[2]]
+
 config_path = f"../datasets/{sys.argv[2]}.json"
 model_name = sys.argv[2]
+
+os.chdir('./datasets')
 
 os.chdir('../vits')
 command = ["python", "train_ms.py", "-c", config_path, "-m", model_name]
