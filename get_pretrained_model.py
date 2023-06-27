6f339d3cddf857b161c3182d75b80208e3abe69a
ORI-Muchim <whals467746@gmail.com>
2023-06-28 00:56:51 +0900

    Edit get_pretrained_model.py

--- get_pretrained_model.py
+++ get_pretrained_model.py
@@ -5,7 +5,13 @@ import os
 
 url1 = 'https://github.com/ORI-Muchim/PolyLangVITS/releases/download/v1.0/D_0.pth'
 url2 = 'https://github.com/ORI-Muchim/PolyLangVITS/releases/download/v1.0/G_0.pth'
+
+print("Downloading Discriminator Model...")
+
 response1 = requests.get(url1, allow_redirects=True)
+
+print("Downloading Generator Model...")
+
 response2 = requests.get(url2, allow_redirects=True)
 
 directory = f'./models/{sys.argv[2]}'
@@ -16,12 +22,12 @@ if not os.path.exists(directory):
 discriminator_model = os.path.join(directory, 'D_0.pth')
 generator_model = os.path.join(directory, 'G_0.pth')
 
-print("Downloading Discriminator Model...")
-
 with open(discriminator_model, 'wb') as file:
     file.write(response1.content)
 
-print("Downloading Generator Model...")
+print("Saving Discriminator Model...")
 
 with open(generator_model, 'wb') as file:
     file.write(response2.content)
+
+print("Saving Generator Model...")
