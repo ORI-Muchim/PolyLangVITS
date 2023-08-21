b164248ae8b397ada0623b4c38ba64929d4e7785
ORI-Muchim <whals467746@gmail.com>
2023-08-21 14:18:44 +0900

    Edit main.py

--- get_pretrained_model.py
+++ get_pretrained_model.py
@@ -2,32 +2,36 @@ import requests
 import sys
 import os
 
+gen = f"./models/{sys.argv[2]}/G_0.pth"
 
-url1 = 'https://github.com/ORI-Muchim/PolyLangVITS/releases/download/v1.0/D_0.pth'
-url2 = 'https://github.com/ORI-Muchim/PolyLangVITS/releases/download/v1.0/G_0.pth'
+if not os.path.isfile(gen):
+    url1 = 'https://github.com/ORI-Muchim/PolyLangVITS/releases/download/v1.0/D_0.pth'
+    url2 = 'https://github.com/ORI-Muchim/PolyLangVITS/releases/download/v1.0/G_0.pth'
 
-print("Downloading Discriminator Model...")
+    print("Downloading Discriminator Model...")
 
-response1 = requests.get(url1, allow_redirects=True)
+    response1 = requests.get(url1, allow_redirects=True)
 
-print("Downloading Generator Model...")
+    print("Downloading Generator Model...")
 
-response2 = requests.get(url2, allow_redirects=True)
+    response2 = requests.get(url2, allow_redirects=True)
 
-directory = f'./models/{sys.argv[2]}'
+    directory = f'./models/{sys.argv[2]}'
 
-if not os.path.exists(directory):
-    os.makedirs(directory)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-discriminator_model = os.path.join(directory, 'D_0.pth')
-generator_model = os.path.join(directory, 'G_0.pth')
+    discriminator_model = os.path.join(directory, 'D_0.pth')
+    generator_model = os.path.join(directory, 'G_0.pth')
 
-with open(discriminator_model, 'wb') as file:
-    file.write(response1.content)
+    with open(discriminator_model, 'wb') as file:
+        file.write(response1.content)
 
-print("Saving Discriminator Model...")
+    print("Saving Discriminator Model...")
 
-with open(generator_model, 'wb') as file:
-    file.write(response2.content)
+    with open(generator_model, 'wb') as file:
+        file.write(response2.content)
 
-print("Saving Generator Model...\n")
+    print("Saving Generator Model...\n")
+else:
+    print('Skipping Download... Model exists.')
--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import os
 import subprocess
 import sys
 
+#sys.argv[1] = ex) ko, ja, en, zh
+#sys.argv[2] = ALL transcription saved in this txt. insert model name
+#sys.argv[3] = sample rate
+
+sys.argv[1] = sys.argv[1].lower()
+
 os.chdir('./datasets')
 subprocess.run(["python", "integral.py", sys.argv[1], sys.argv[2], sys.argv[3]])
 
