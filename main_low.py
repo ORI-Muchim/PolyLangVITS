e98003501ae580fd9999d8ec666afc6a576a2f84
ORI-Muchim <whals467746@gmail.com>
2023-09-16 14:33:38 +0900

    Edit main.py

--- main_low.py
+++ main_low.py
@@ -6,17 +6,22 @@ import sys
 #sys.argv[2] = ALL transcription saved in this txt. insert model name
 #sys.argv[3] = sample rate
 
-sys.argv[1] = sys.argv[1].lower()
+if len(sys.argv) < 4:
+    print("Usage: script_name <language> <model_name> <sample_rate>")
+    sys.exit(1)
+
+language = sys.argv[1].lower()
+model_name = sys.argv[2]
+sample_rate = int(sys.argv[3])
 
 os.chdir('./datasets')
-subprocess.run(["python", "integral_low.py", sys.argv[1], sys.argv[2], sys.argv[3]])
+subprocess.run(["python", "integral_low.py", language, model_name, str(sample_rate)])
 
 os.chdir('../')
-subprocess.run(["python", "get_pretrained_model.py", sys.argv[1], sys.argv[2]])
+if sample_rate == 44100:
+    subprocess.run(["python", "get_pretrained_model.py", language, model_name])
 
-config_path = f"../datasets/{sys.argv[2]}.json"
-model_name = sys.argv[2]
+config_path = f"../datasets/{model_name}.json"
 
 os.chdir('./vits')
 subprocess.run(["python", "train_ms.py", "-c", config_path, "-m", model_name])
-
